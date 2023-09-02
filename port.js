c8ce954a3ae322a9d5b3cc536522ffbc426a97f0
shafihi <shafhikasim@yahoo.com>
2023-09-02 10:08:16 +0100

    Website Updated with JavaScript

---
+++ port.js
@@ -0,0 +1,36 @@
+
+const toggleListVisibility = (list) => {
+    if (list.style.display === 'none') {
+        list.style.display = 'block'; // Show the list
+    } else {
+        list.style.display = 'none';  // Hide the list
+    }
+}
+
+const hideList = (id, list) => {
+    id.onclick = () => {
+        toggleListVisibility(list); // Toggle list visibility when the button is clicked
+    }
+}
+
+if (typeof window !== 'undefined') {
+    // This code will only run in a browser environment
+    window.onload = function() {
+        // Your code that manipulates the DOM goes here
+        const wxp1 = document.getElementById('wxp1');
+        const wxp2 = document.getElementById('wxp2');
+        const wxp3 = document.getElementById('wxp3');
+        const wxp4 = document.getElementById('wxp4');
+
+        const aclist = document.getElementById('aclist');
+        const vmlist = document.getElementById('vmlist');
+        const xblist = document.getElementById('xblist');
+        const jrllist = document.getElementById('jrllist');
+
+        hideList(wxp1 ,aclist);
+        hideList(wxp2 ,vmlist);
+        hideList(wxp3 ,xblist);
+        hideList(wxp4 ,jrllist);     
+    }
+}
+
